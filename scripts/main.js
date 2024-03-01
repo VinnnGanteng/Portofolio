1638c3454fef3f759e682d6878293871c85d24dc
VinnnGanteng <161796010+VinnnGanteng@users.noreply.github.com>
2024-03-01 11:22:25 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+
+
+// Toogle Res
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+}
+
+onload = ()=>{
+    navSlide();
+}
+
+
+
+
+// clear
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    };
+};
